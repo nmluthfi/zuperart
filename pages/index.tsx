c0992caf3815e361f0e958849d8317675410efc4
Nur Muhammad Luthfi <nurmuhammadluthfi@gmail.com>
2023-10-11 16:34:29 +0700

    remove unnecessary things

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,28 +12,6 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <div className={styles.content}>
         <div className={styles.hero}>
-          {/* <div className={styles.heroBackground}>
-            <div className={styles.heroBackgroundInner}>
-              <Image
-                src="/hero-gradient.png"
-                width={1980}
-                height={1280}
-                alt="Background gradient from red to blue"
-                quality={100}
-                className={styles.gradient}
-              />
-            </div>
-          </div> */}
-          {/* <div className={styles.heroAssetFrame}>
-            <Image
-              src="/hero-asset.png"
-              width={860}
-              height={540}
-              alt="Hero asset, NFT marketplace"
-              quality={100}
-              className={styles.heroAsset}
-            />
-          </div> */}
           <div className={styles.heroBodyContainer}>
             <div className={styles.heroBody}>
               <h1 className={styles.heroTitle}>
@@ -50,7 +28,7 @@ const Home: NextPage = () => {
                 </Link>
                 {/* <Link
                   className={styles.secondaryCta}
-                  href="https://github.com/thirdweb-example/marketplace-v3"
+                  href="#"
                   target="_blank"
                 >
                   Join our community
