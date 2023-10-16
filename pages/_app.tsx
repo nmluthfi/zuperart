43bbb9b1736625bf59792bcaf21e26a8f39f5eb3
Nur Muhammad Luthfi <nurmuhammadluthfi@gmail.com>
2023-10-16 15:55:58 +0700

    change the faucet url to a placeholder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
-      activeChain={NETWORK}
+      activeChain={{...NETWORK, faucets: ["#"]}}
       supportedWallets={[
         metamaskWallet({ recommended: true }),
         walletConnect(),
